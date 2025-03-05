6b61804c0b6b5e03e495f46b33ae3a1b9d0d51cd
Danylo <danylohenrique2407@gmail.com>
2025-03-05 11:42:08 -0300

    chore(config) adicionar arquivo test.config.jsl

---
+++ test.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest', // Transforma arquivos TS e TSX
+    },
+    globals: {
+      'ts-jest': {
+        isolatedModules: true, // Pode ajudar em alguns casos
+      },
+    },
+  };
+  
