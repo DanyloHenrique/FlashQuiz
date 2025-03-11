34d67d54da4571a38f48e87eeb66bb5679fd28f2
Danylo <danylohenrique2407@gmail.com>
2025-03-11 00:14:30 -0300

    feat: adição do userId no Quiz

--- src/domain/dto/quiz.model.DTO.ts
+++ src/domain/dto/quiz.model.DTO.ts
@@ -2,6 +2,7 @@ import { Visibility } from "../model/quiz.model";
 import { FlashcardDTO } from "./flashcard.model.DTO";
 
 export interface QuizDTO {
+  userId: string;
   title: string;
   description?: string;
   flashCard?: FlashcardDTO[];
